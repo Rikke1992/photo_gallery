0731c08f602de081116c1b2031a5fe1932f3dab7
Rikke1992 <vladimirp300@gmail.com>
2020-09-06 22:15:10 +0300

    initial new function

--- script/app.js
+++ script/app.js
@@ -1,75 +1,54 @@
 
-// var request = new XMLHttpRequest ()
-// console.log (request.open (
-//   'GET',
-//   'https://jsonplaceholder.typicode.com/users'
-// ))
-const inputEl = document.getElementById('file-selector');
 const wraperContent = document.getElementById('wraperContent');
-let gepaButton = document.getElementById('gepaButton');
-let gepaImage = document.getElementById('gepaImage');
-let n = 1;
-let ten = 10;
+let button = document.getElementById('gepaButton');
+let ferstVariable = 1;
+let secondVariable = 10;
 
-window.addEventListener('scroll', ()=>{
-  populate()
 
-} )
 
-gepaButton.addEventListener('click', ()=>{
+button.addEventListener('click', () => {
 
   fetch('https://jsonplaceholder.typicode.com/photos')
-    .then((response)=>{
-      return response.json();})
-    .then(data=>{
-      populate()
-      for (n; n<= ten ; n++) {
-        console.log(data[n].thumbnailUrl)
-        wraperContent.insertAdjacentHTML('beforeend', `
+    .then((response) => {
+      return response.json();
+    })
+    .then(data => {
+      check(ferstVariable, secondVariable,data)
+
+    });
+
+});
+
+window.addEventListener('scroll', () => {
+  fetch('https://jsonplaceholder.typicode.com/photos')
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      let windowRelativeBottom = document.documentElement.getBoundingClientRect().bottom;
+
+      // если пользователь прокрутил достаточно далеко (< 100px до конца)
+      if (windowRelativeBottom < document.documentElement.clientHeight + 25) {
+        // добавим больше данных
+
+        check(ferstVariable, secondVariable, data)
+
+      }
+    });
+
+});
+
+
+function check (ferstVariable, secondVariable, data) {
+ for (ferstVariable; ferstVariable <= secondVariable; ferstVariable++) {
+
+    wraperContent.insertAdjacentHTML('beforeend', `
         <div>
-        <img src="${data[n].thumbnailUrl}">
+        <img src="${data[ferstVariable].thumbnailUrl}">
         </div>
-        `)
-
-      }
-      ten = ten +10
-    })
-
-})
-
-window.addEventListener('scroll', ()=>{
-  fetch('https://jsonplaceholder.typicode.com/photos')
-    .then((response)=>{
-      return response.json();})
-    .then((data)=>{
-      for (n; n<= ten ; n++) {
-        wraperContent.insertAdjacentHTML('beforeend', `
-      <div>
-        <img src="${data[1].thumbnailUrl}">
-        </div>
-      `)
-      }
-      ten = ten +10
-    })
-
-})
-
-
-
-
-
-
-
-
-function populate() {
-
-
-    let windowRelativeBottom = document.documentElement.getBoundingClientRect().bottom;
-
-    // если пользователь прокрутил достаточно далеко (< 100px до конца)
-    if (windowRelativeBottom < document.documentElement.clientHeight + 25) {
-      // добавим больше данных
-      console.log('opaa')
+        `);
 
   }
+  secondVariable = secondVariable + 1;
+console.log(secondVariable, ferstVariable )
 }
