2e4289c685a4b4d678a6a785bf3dee5ad39a373a
Rikke1992 <vladimirp300@gmail.com>
2020-09-19 18:01:43 +0300

    initial API carousel

--- script/app.js
+++ script/app.js
@@ -1,24 +1,30 @@
 
 const wraperContent = document.getElementById('wraperContent');
 let button = document.getElementById('gepaButton');
-let imageId = 1;
-let imageNum = 10;
-let id1 = document.getElementById('1');
-let id2 = document.getElementById('2');
-let id3 = document.getElementById('3');
-let id4 = document.getElementById('4');
+let imageId = 0;
+let imageNum = 9;
 let left = document.getElementById('left');
 let right = document.getElementById('right');
 let carouselImg = document.getElementById('carouselImg');
 let quantityMargins = 0;
-let lengthImages = document.querySelectorAll('img').length;
+let lengthImages = document.querySelectorAll('.imageIcon').length;
 let colImagSwap = (lengthImages - 4)* 150;
+let res = [];
+
+
+
+
+
 
 left.addEventListener('click', ()=>{
   quantityMargins -= 150;
   Math.abs(quantityMargins) > colImagSwap ? quantityMargins+=150 : swapCarousel();
+  console.log('left')
 
 })
+
+
+
 
 right.addEventListener('click', ()=>{
   quantityMargins += 150;
@@ -28,59 +34,93 @@ right.addEventListener('click', ()=>{
 })
 
 
-
 function swapCarousel (){
 
 
-  let carouselMargin= carouselImg.style.margin = `0 0 0 ${quantityMargins}px`;
+ let carouselMargin= carouselImg.style.margin = `0 0 0 ${quantityMargins}px`;
 
+  return carouselMargin
 
 }
 
 
 
+button.addEventListener('click', async function EventButton(){
 
-button.addEventListener('click', () => {
+    let fetches = await fetch('https://jsonplaceholder.typicode.com/photos');
+    let jsone = await fetches.json();
 
-  fetch('https://jsonplaceholder.typicode.com/photos')
-    .then((response) => {
-      return response.json();
-    })
-    .then(data => {
-      check(imageId, imageNum,data)
+    printImageFromSite(imageId, imageNum,jsone)
+    imageId+=10;
+    imageNum+=10
+    lengthImages = document.querySelectorAll('.imageIcon').length;
+    colImagSwap = (lengthImages - 4)* 150;
+  }
+//   () => {
+//
+//   fetch('https://jsonplaceholder.typicode.com/photos')
+//     .then((response) => {
+//       return response.json();
+//     })
+//     .then(data => {
+//
+//       printImageFromSite(imageId, imageNum,data)
+//       imageId+=10;
+//       imageNum+=10
+//
+//
+//     });
+//
+// }
+);
 
-    });
-
-});
-
-window.addEventListener('scroll', () => {
-  fetch('https://jsonplaceholder.typicode.com/photos')
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      let windowRelativeBottom = document.documentElement.getBoundingClientRect().bottom;
-
-      // если пользователь прокрутил достаточно далеко (< 100px до конца)
-      if (windowRelativeBottom < document.documentElement.clientHeight + 25) {
-        // добавим больше данных
-
-        check(imageId, imageNum, data)
-
-      }
-    });
-
-});
+// window.addEventListener('scroll', () => {
+//   fetch('https://jsonplaceholder.typicode.com/photos')
+//     .then((response) => {
+//       return response.json();
+//     })
+//     .then((data) => {
+//       let windowRelativeBottom = document.documentElement.getBoundingClientRect().bottom;
+//
+//       // если пользователь прокрутил достаточно далеко (< 25px до конца)
+//       if (windowRelativeBottom < document.documentElement.clientHeight + 25) {
+//         // добавим больше данных
+//
+//         check(imageId, imageNum, data)
+//
+//       }
+//     });
+//
+// });
 
 
-function check (imageId, imageNum, data) {
+// function check (imageId, imageNum, data) {
+//  for (imageId; imageId <= imageNum; imageId++) {
+//
+//     wraperContent.insertAdjacentHTML('beforeend', `
+//         <div class="data[imageId].id">
+//         <p>${data[imageId].id}</p>
+//         <img src="${data[imageId].thumbnailUrl}">
+//         </div>
+//         `);
+//
+//
+//   }
+//
+//
+// }
+
+function printImageFromSite (imageId, imageNum, data) {
  for (imageId; imageId <= imageNum; imageId++) {
 
-    wraperContent.insertAdjacentHTML('beforeend', `
-        <div>
-        <img src="${data[imageId].thumbnailUrl}">
+   carouselImg.insertAdjacentHTML('beforeend', `
+        <div class="imageIcon">
+                <img src="${data[imageId].thumbnailUrl}">
         </div>
         `);
+
+
+
 
   }
 
